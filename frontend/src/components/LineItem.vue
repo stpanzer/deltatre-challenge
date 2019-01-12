85e302a2cd73b3d602b186966cd787be3c7d17ea
Stephen Panzer <stephen@flyutahair.com>
2019-01-12 15:39:01 -0700

    Simple display of line items

---
+++ frontend/src/components/LineItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{item.name}}
+    {{item.description}}
+    {{item.price | currency}}
+    {{item.quantity}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LineItem',
+  props: ['item']
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,5 +1,15 @@
 <template>
-    <div></div>
+    <div>
+      <div v-if="lineItems && lineItems.length > 0">
+        <div v-for="lineItem of lineItems" v-bind:key="lineItem.id">
+          <line-item :item=lineItem></line-item>
+        </div>
+      </div>
+      <div v-else>
+        Sorry, there aren't any products to show at this time.
+      </div>
+
+    </div>
 </template>
 
 <script>
@@ -8,10 +18,17 @@ export default {
   name: 'ProductList',
   data () {
     return {
-      lineItems: []
+      lineItems: [
+        {
+          name: 'Rose Bouquet',
+          description: 'An simple bouquet',
+          price: '14.99',
+          quantity: 20
+        }
+      ]
     }
   },
-  components: [LineItem]
+  components: { LineItem }
 }
 </script>
 
