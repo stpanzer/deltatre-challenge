83806144cfe2579d3c80566e7117000cff35cb2b
Stephen Panzer <stephen@flyutahair.com>
2019-01-12 15:38:42 -0700

    Add and register a currency filter

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueCurrencyFilter from 'vue-currency-filter'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCurrencyFilter,
+  {
+    symbol: '$',
+    thousandsSeparator: ',',
+    fractionCount: 2,
+    fractionSeparator: '.',
+    symbolPosition: 'front'
+  })
 
 new Vue({
   router,
